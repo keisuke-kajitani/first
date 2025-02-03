9293ee7fa6f8bd41f5f28e789cc27a74ba6be8ef
kajitanikeisuke <keisuke.kajitani@rakus-partners.co.jp>
2025-02-03 16:27:50 +0900

    2

--- emp_management/src/main/java/com/example/controller/AdministratorController.java
+++ emp_management/src/main/java/com/example/controller/AdministratorController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import jakarta.servlet.http.HttpSession;
 import com.example.domain.Administrator;
@@ -22,6 +23,7 @@ import com.example.service.AdministratorService;
  * @author igamasayuki
  *
  */
+@SessionAttributes("administrator")
 @Controller
 @RequestMapping("/")
 public class AdministratorController {
